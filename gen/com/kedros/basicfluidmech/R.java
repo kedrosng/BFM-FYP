ebe98fe893dfb0ee8c24775d1289bcf44fe521c8
kedrosng <Kedros@kedros-mbp.local>
2014-08-12 20:57:13 +0800

    20140812a
    setcontentview and add bg test.
    Seems fine

--- gen/com/kedros/basicfluidmech/R.java
+++ gen/com/kedros/basicfluidmech/R.java
@@ -1084,6 +1084,7 @@ containing a value of this type.
         public static final int unnamed=0x7f02005a;
     }
     public static final class id {
+        public static final int ScrollView01=0x7f05003c;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1092,12 +1093,12 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_help=0x7f05003e;
+        public static final int action_help=0x7f05003f;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_refresh=0x7f05003f;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_refresh=0x7f050040;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1116,7 +1117,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imgLogo=0x7f05003c;
+        public static final int imgLogo=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1250,6 +1251,7 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int about=0x7f0a0014;
         public static final int action_help=0x7f0a0010;
         public static final int action_refresh=0x7f0a0011;
         public static final int action_settings=0x7f0a000f;
