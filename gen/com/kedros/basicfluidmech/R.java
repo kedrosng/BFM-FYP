0b19ea4d60d89e3b232ec1cf5feeef391d09756e
kedrosng <kedrosng@gmail.com>
2014-10-10 23:44:09 +0800

    20141010
    Code Backup.
    Try making formula

--- gen/com/kedros/basicfluidmech/R.java
+++ gen/com/kedros/basicfluidmech/R.java
@@ -1099,13 +1099,13 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_exit=0x7f050051;
-        public static final int action_help=0x7f050050;
+        public static final int action_exit=0x7f050052;
+        public static final int action_help=0x7f050051;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_search=0x7f05004e;
-        public static final int action_settings=0x7f05004f;
+        public static final int action_search=0x7f05004f;
+        public static final int action_settings=0x7f050050;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int basic_c1=0x7f050045;
@@ -1114,7 +1114,7 @@ containing a value of this type.
         public static final int basic_t2=0x7f050046;
         public static final int basic_t3=0x7f050048;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05004d;
+        public static final int button1=0x7f05004e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1132,7 +1132,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f05004b;
+        public static final int imageView1=0x7f05004d;
         public static final int imgLogo=0x7f050040;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
@@ -1147,6 +1147,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int reyformula=0x7f05004b;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
--- src/com/kedros/basicfluidmech/NavDrawer3.java
+++ src/com/kedros/basicfluidmech/NavDrawer3.java
@@ -6,19 +6,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
- 
+import android.widget.Toast;
+import android.app.Activity;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
+
 public class NavDrawer3 extends Fragment {
- 
-    public static Fragment newInstance(Context context) {
-    	NavDrawer3 f = new NavDrawer3();
- 
-        return f;
-    }
- 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.navdrawer3, null);
-        return root;
-    }
- 
+
+	public static Fragment newInstance(Context context) {
+		NavDrawer3 f = new NavDrawer3();
+
+		return f;
+	}
+
+	View mView;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// ViewGroup root = (ViewGroup) inflater.inflate(R.layout.navdrawer3,
+		// null);
+		mView = inflater.inflate(R.layout.navdrawer3, container, false);
+
+		return mView;
+		// return root;
+	}
+
+	ImageButton imageButton;
+	Activity activity = getActivity();
+
+	public void addListenerOnButton() {
+
+		imageButton = (ImageButton) mView.findViewById(R.id.reyformula);
+
+		imageButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View arg0) {
+
+				Toast.makeText(getActivity(), "ImageButton is clicked!",
+						Toast.LENGTH_SHORT).show();
+
+			}
+
+		});
+
+	}
 }
